43bdf4cbb883e09c68afcdcfc28c263e45ed7b7a
pnhung177 <pnhung177@acegik.net>
2019-03-13 11:47:24 +0700

    Return error if invalid JSON format provided

--- example.js
+++ example.js
@@ -1,5 +1,13 @@
 var bootstrap = require('./bootstrap');
 
 bootstrap(function(err, store) {
+  if (err) {
+    process.stderr.write(JSON.stringify({
+      name: err.name,
+      message: err.message,
+      stack: err.stack
+    }, null, 2));
+    process.exit(1);
+  }
   console.log(JSON.stringify(store, null, 2));
 })
