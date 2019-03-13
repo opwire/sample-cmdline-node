8a18b2168a55f5c8e42f5404ca15c89614624d3f
pnhung177 <pnhung177@acegik.net>
2019-03-13 13:25:13 +0700

    Customize output format with command line arguments

--- example.js
+++ example.js
@@ -1,13 +1,34 @@
+const minimist = require('minimist');
 var bootstrap = require('./bootstrap');
 
 bootstrap(function(err, store) {
+  const argv = minimist(process.argv.slice(2));
+  const isPlainText = argv.format === "text";
   if (err) {
-    process.stderr.write(JSON.stringify({
-      name: err.name,
-      message: err.message,
-      stack: err.stack
-    }, null, 2));
+    if (isPlainText) {
+      process.stderr.write(err);
+    } else {
+      process.stderr.write(JSON.stringify({
+        name: err.name,
+        message: err.message,
+        stack: err.stack
+      }, null, 2));
+    }
+    process.stderr.write("\n");
     process.exit(1);
   }
-  console.log(JSON.stringify(store, null, 2));
+
+  if (isPlainText) {
+    for(const key in store) {
+      console.log("%s:", key.toUpperCase());
+      let content = store[key];
+      if (typeof store[key] !== 'string') {
+        content = JSON.stringify(content, null, 2);
+      }
+      console.log(content);
+      console.log();
+    }
+  } else {
+    console.log(JSON.stringify(store, null, 2));
+  }
 })
